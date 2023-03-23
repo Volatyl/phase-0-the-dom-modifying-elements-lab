f397414f038a617ab78c96bbdc57c28bd1800a64
Kennedy Muinde <kennedymuinde@gmail.com>
2023-03-23 07:37:51 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ const newHeader = document.createElement("h1");
 newHeader.id = "victory";
 newHeader.textContent = "Ken is the champion";
 document.body.appendChild(newHeader);
+
+const element = document.createElement("div");
+document.body.append(element);
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
